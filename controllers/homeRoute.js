3bbf0ef0d357a5a9012a21e07f14846a2518409f
Juri <jasonluxie@gmail.com>
2021-11-01 18:51:21 -0500

    home route

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,14 +1,15 @@
-const router = require('express').Router();
-const Blog = require('../model/Blog')
+const router = require("express").Router();
+const { User, Blog } = require("../model/index");
 
-router.get('/', async (req, res) => { //This route is probably wrong too
-    try{
-        const blogData = await Blog.findAll()
-        const blogPosts = blogData.map((data) => data.get({plain : true}))
-        res.render('all', {}) //Need to figure this out 
-    } catch(err) {
-        res.status(500).json(`There seems to be some sort of database error`)
+router.get("/", async (req, res) => {
+    try {
+        const blogData = await Blog.findAll();
+        const blogPosts = blogData.map((data) => data.get({ plain: true }));
+        res.render("homepage", { blogPosts, loggedIn: req.session.loggedIn });
+    } catch (err) {
+        res.status(500).json(`There seems to be some sort of database error`);
+        console.error(err);
     }
-  });
-  
-  module.exports = router;
+});
+
+module.exports = router;
