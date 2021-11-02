17a98e26c05feaecdf4730ed1a38c8f243b47557
Juri <jasonluxie@gmail.com>
2021-11-01 20:54:56 -0500

    named

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const userRoute = require("./userRoute");
-// const postRoute = require("./postRoute");
+const blogRoute = require("./blogRoute");
 // const commentRoute = require("./commentRoute");
 
 router.use("/users", userRoute);
-// router.use("/posts", postRoute);
+router.use("/posts", blogRoute);
 // router.use("/comments", commentRoute);
 
 module.exports = router;
