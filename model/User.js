0382c3a756b39f39a325c994bbd3856f00db4a8f
Juri <jasonluxie@gmail.com>
2021-11-09 09:30:34 -0600

    hooks broke code

--- model/User.js
+++ model/User.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const sequelize = require("../config/connection.js");
+const sequelize = require("../config/connection");
 const { Model, DataTypes } = require("sequelize");
 
 class User extends Model {}
@@ -29,24 +29,24 @@ User.init(
             notNull: true,
         },
     },
-    {
-        hooks: {
-            beforeCreate: async (userDataNew) => {
-                userDataNew.password = await bcrypt.hash(
-                    userDataNew.password,
-                    10
-                );
-                return userDataNew;
-            },
-            beforeUpdate: async (userDataUpdate) => {
-                userDataUpdate.password = await bcrypt.hash(
-                    userDataUpdate.password,
-                    10
-                );
-                return userDataUpdate;
-            },
-        },
-    },
+    // {
+    //     hooks: {
+    //         beforeCreate: async (userDataNew) => {
+    //             userDataNew.password = await bcrypt.hash(
+    //                 userDataNew.password,
+    //                 10
+    //             );
+    //             return userDataNew;
+    //         },
+    //         beforeUpdate: async (userDataUpdate) => {
+    //             userDataUpdate.password = await bcrypt.hash(
+    //                 userDataUpdate.password,
+    //                 10
+    //             );
+    //             return userDataUpdate;
+    //         },
+    //     },
+    // },
     {
         sequelize,
         timestamps: false,
